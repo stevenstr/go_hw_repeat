39164df41aaf34f0f383e2efff079077cea49218
Ste Zha <stephenzhavrid@gmail.com>
2020-01-27 01:35:35 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 /*
  *Author: Stefan
- *Date: 01/26/2020
- *Last changes: 01/26/2020 23:35
- *Task: HW 1.1 Hello World
+ *Date: 01/27/2020
+ *Last changes: 01/27/2020 01:35
+ *Task: HW 2.2 Square
  */
 
 package main
 
 import "fmt"
 
+//Point type
+type Point struct {
+	x, y int
+}
+
+//Square type
+type Square struct {
+	start Point
+	a     uint
+}
+
+//End method
+func (s Square) End() (p Point) {
+	p.x = s.start.x + int(s.a)
+	p.y = s.start.y + int(s.a)
+	return p
+}
+
+//Perimeter method
+func (s Square) Perimeter() (p uint) {
+	p = s.a * uint(4)
+	return p
+}
+
+//Area method
+func (s Square) Area() (ar uint) {
+	ar = s.a * s.a
+	return ar
+}
+
+//main func
 func main() {
-	fmt.Println("Hello Golang world!")
+	s := Square{Point{1, 1}, 5}
+	fmt.Println(s.End())
+	fmt.Println(s.Perimeter())
+	fmt.Println(s.Area())
 }
