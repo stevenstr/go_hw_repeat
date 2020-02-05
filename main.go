ffbfa90845095b7206ac92f4bc3af34801489e1b
Ste Zha <stephenzhavrid@gmail.com>
2020-02-05 20:43:22 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ *Author: Stefan
+ *Date: 02/05/2020
+ *Last changes: 02/05/2020 20:42
+ *Task: HW 4.3 Smile
+ */
+
+package main
+
+import "github.com/kyokomi/emoji"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		emoji.Println("Hello world :smile:!")
+	}
+}
