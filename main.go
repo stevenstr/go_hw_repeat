41b91cd681e22501fb60fff18401d97da08ec852
Ste Zha <stephenzhavrid@gmail.com>
2020-02-06 13:23:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 /*
  *Author: Stefan
  *Date: 02/05/2020
- *Last changes: 02/06/2020 12:57
- *Task: Concurrency 1 Checker
+ *Last changes: 02/06/2020 13:23
+ *Task: Concurrency 2 Checker
  */
 
 package main
@@ -10,6 +10,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -25,33 +26,45 @@ func main() {
 	c := make(chan string)
 
 	//cicle start in main goroutine
+	//just init
 	for _, url := range resources {
 		go checkRes(url, c) //create new goroutine
 	}
-	//waiting value from channel
-	/*
-		fmt.Println(<-c) //this operation are blocking!! and print facter answer
-		fmt.Println(<-c)
-		fmt.Println(<-c)
-		fmt.Println(<-c)
-		fmt.Println(<-c)
-		//fmt.Println(<-c)// dead lock of main routine
+	//read from channel
+	//and create new goroutines^))))))
+	/*for {
+		go checkRes(<-c, c)
+	}
 	*/
-
-	//waiting value from channel
-	for range resources {
-		fmt.Println(<-c)
+	//alternative way
+	/*
+		for l := range c {
+			checkRes(l, c)
+		}
+	*/
+	for l := range c {
+		//bad method, we just stopped main routine
+		//time.Sleep(5 * time.Second)
+		//send parameter so that linking woll be on diff mamory place
+		go func(url string) {
+			time.Sleep(5 * time.Second)
+			checkRes(url, c)
+		}(l)
 	}
 }
 
+//checkRes func
 func checkRes(url string, c chan string) {
+
 	_, err := http.Get(url) //processor wait, GET is block operation
 	if err != nil {
 		//send to channel
-		c <- fmt.Sprintln(url, "is down!")
+		fmt.Println(url, "is down!")
+		c <- url
 		return
 	}
 	//send to channel
-	c <- fmt.Sprintln(url, "is Up!")
+	fmt.Println(url, "is Up!")
+	c <- url
 	return
 }
