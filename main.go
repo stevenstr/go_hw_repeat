9057abec71379f4047c616867a491a9ff22541c1
Ste Zha <stephenzhavrid@gmail.com>
2020-02-06 22:02:09 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 /*
  *Author: Stefan
  *Date: 02/06/2020
- *Last changes: 02/06/2020 21:36
+ *Last changes: 02/06/2020 22:03
  *Task: CW L7
  */
 
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"net"
 )
 
 //main func
 func main() {
+	//Json
 	//ex 1
 	/*
 		//put jsonStr into slice byte
@@ -35,24 +37,42 @@ func main() {
 	*/
 
 	//ex 2
+	/*
+		p := &Pizza{
+			Name:  "Cheese",
+			Price: 15,
+		}
 
-	p := &Pizza{
-		Name:  "Cheese",
-		Price: 15,
-	}
+		//Marshaling struct into json
+		result, err := json.Marshal(p)
+		if err != nil {
+			fmt.Println("Somethong wrong!")
+		}
+		fmt.Println(string(result))
 
-	//Marshaling struct into json
-	result, err := json.Marshal(p)
+		//Unmarshaling json as slice byte into struct
+		res := []byte(result)
+		c := &Pizza{}
+		json.Unmarshal(res, c)
+		fmt.Println(c)
+	*/
+
+	//TCP socket
+	//ex 3
+	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
-		fmt.Println("Somethong wrong!")
+		fmt.Println(err)
 	}
-	fmt.Println(string(result))
 
-	//Unmarshaling json as slice byte into struct
-	res := []byte(result)
-	c := &Pizza{}
-	json.Unmarshal(res, c)
-	fmt.Println(c)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		//goroutines
+		go handleFunction(conn)
+	}
+
 }
 
 //ex 1
@@ -71,7 +91,37 @@ var jsonStr = `{"name":"Cheese", "diameter": 35, "weight":500}`
 */
 
 //ex 2
+/*
 type Pizza struct {
 	Name  string `json:"pizza_name"`
 	Price int    `json:"price"`
+}
+*/
+
+//ex 3
+
+func handleFunction(conn net.Conn) {
+	name := conn.RemoteAddr().String()
+
+	//send to service console
+	fmt.Printf("%v connected\n\r\n", name)
+	//send to client console
+	conn.Write([]byte("Hello, " + name + "\n\r"))
+
+	defer conn.Close()
+
+	//create scanner
+	scanner := bufio.NewScanner(conn)
+
+	//scaning inputs
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "Exit" {
+			conn.Write([]byte("Bye\n\r\n"))
+			break
+		} else if text != "" {
+			fmt.Println(name, "enters", text)
+			conn.Write([]byte("You enter: " + text + "\n\r\n"))
+		}
+	}
 }
