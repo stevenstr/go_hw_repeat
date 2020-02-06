6d46de34509d2b1e718c434f9a2051c781da764d
Ste Zha <stephenzhavrid@gmail.com>
2020-02-06 15:53:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,14 @@
  *Author: Stefan
  *Date: 02/05/2020
  *Last changes: 02/06/2020 15:25
- *Task: Concurrency 4
+ *Task: Concurrency 4 Patterns
  */
 
 package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -51,43 +52,53 @@ func main() {
 
 	//patterns
 	//ex 3
+
+	basicSR()
+	fmt.Println()
+
+	signalClose()
+	fmt.Println()
+
+	signalAck()
+	fmt.Println()
+
+	closeRange()
+	fmt.Println()
+
+	selectRecv()
+	fmt.Println()
+
+	selectSend()
+	fmt.Println()
+
+	selectDrop()
+	fmt.Println()
+	fmt.Println()
+
+	//ex 4 LOOP4
 	/*
-		basicSR()
-		fmt.Println()
+	   	//create chan buff size 2
+	   	ch1 := make(chan int, 2)
+	   	//send val
+	   	ch1 <- 1
+	   	ch1 <- 2
 
-		sigClose()
-		fmt.Println()
-
-		signalAck()
-		fmt.Println()
-
-		closeRange()
-		fmt.Println()
-
-		selectRecv()
+	   	ch2 := make(chan int, 2)
+	   	ch2 <- 3
+	   	//for loop with ability to stop it
+	   	//scheduler make decission who first and who sec
+	   LOOP:
+	   	for {
+	   		select {
+	   		case v1 := <-ch1:
+	   			fmt.Println(v1, "chan1 val")
+	   		case v2 := <-ch2:
+	   			fmt.Println(v2, "chan2 val")
+	   		default:
+	   			break LOOP
+	   		}
+	   	}
 	*/
-	//ex 4 LOOP
-	//create chan buff size 2
-	ch1 := make(chan int, 2)
-	//send val
-	ch1 <- 1
-	ch1 <- 2
-
-	ch2 := make(chan int, 2)
-	ch2 <- 3
-	//for loop with ability to stop it
-	//scheduler make decission who first and who sec
-LOOP:
-	for {
-		select {
-		case v1 := <-ch1:
-			fmt.Println(v1, "chan1 val")
-		case v2 := <-ch2:
-			fmt.Println(v2, "chan2 val")
-		default:
-			break LOOP
-		}
-	}
 }
 
 func basicSR() {
@@ -101,7 +112,8 @@ func basicSR() {
 	fmt.Println(<-ch)
 }
 
-func sigClose() {
+//
+func signalClose() {
 	ch := make(chan struct{})
 	go func() {
 		time.Sleep(100 * time.Microsecond)
@@ -112,6 +124,7 @@ func sigClose() {
 	fmt.Println("event received")
 }
 
+//signalAck func
 func signalAck() {
 	ch := make(chan string)
 	//child gorout
@@ -124,6 +137,7 @@ func signalAck() {
 	fmt.Println(<-ch)
 }
 
+//closeRange func
 func closeRange() {
 	//create box with 5 int
 	ch := make(chan int, 5)
@@ -153,4 +167,40 @@ func selectRecv() {
 	default:
 		fmt.Println("Default case")
 	}
+}
+
+//selectSend func
+func selectSend() {
+	ch := make(chan string)
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+	}()
+
+	select {
+	case ch <- "work":
+		fmt.Println("send work")
+	case <-time.After(100 * time.Millisecond):
+		fmt.Println("time out")
+	}
+}
+
+//selectDrop func
+func selectDrop() {
+	ch := make(chan int, 5)
+
+	go func() {
+		for v := range ch {
+			fmt.Println("recv", v)
+		}
+	}()
+
+	for i := 0; i < 20; i++ {
+		select {
+		case ch <- i:
+		default:
+			fmt.Println("drop", i)
+		}
+	}
+	close(ch)
+	fmt.Scanln()
 }
