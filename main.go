690a4666637b4417c64f24b2e88cff466977f208
Ste Zha <stephenzhavrid@gmail.com>
2020-02-06 21:36:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,51 @@ import (
 	"fmt"
 )
 
+//main func
+func main() {
+	//ex 1
+	/*
+		//put jsonStr into slice byte
+		data := []byte(jsonStr)
+
+		p := &Pizza{}
+
+		//Unmarshal our byte slice into p struct
+		json.Unmarshal(data, p)
+		//print our unmarshaled struct p
+		fmt.Println(p)
+
+		//Marshaling our struct into json
+		pp, err := json.Marshal(p)
+		if err != nil {
+			fmt.Println("Something wrong!")
+		}
+		fmt.Println(string(pp))
+	*/
+
+	//ex 2
+
+	p := &Pizza{
+		Name:  "Cheese",
+		Price: 15,
+	}
+
+	//Marshaling struct into json
+	result, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println("Somethong wrong!")
+	}
+	fmt.Println(string(result))
+
+	//Unmarshaling json as slice byte into struct
+	res := []byte(result)
+	c := &Pizza{}
+	json.Unmarshal(res, c)
+	fmt.Println(c)
+}
+
+//ex 1
+/*
 //Pizza struct
 type Pizza struct {
 	Name     string
@@ -23,22 +68,10 @@ type Pizza struct {
 
 //serialization of struct in json format
 var jsonStr = `{"name":"Cheese", "diameter": 35, "weight":500}`
+*/
 
-func main() {
-	//put jsonStr into slice byte
-	data := []byte(jsonStr)
-
-	p := &Pizza{}
-
-	//Unmarshal our byte slice into p struct
-	json.Unmarshal(data, p)
-	//print our unmarshaled struct p
-	fmt.Println(p)
-
-	//Marshaling our struct into json
-	pp, err := json.Marshal(p)
-	if err != nil {
-		fmt.Println("Something wrong!")
-	}
-	fmt.Println(string(pp))
+//ex 2
+type Pizza struct {
+	Name  string `json:"pizza_name"`
+	Price int    `json:"price"`
 }
