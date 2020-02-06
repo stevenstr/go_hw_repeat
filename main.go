15c6f82c9a05abd9507d37ab22a5f91d4f6d48c4
Ste Zha <stephenzhavrid@gmail.com>
2020-02-06 13:36:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,53 @@
 /*
  *Author: Stefan
- *Date: 01/26/2020
- *Last changes: 01/26/2020 23:35
- *Task: HW 1.1 Hello World
+ *Date: 02/06/2020
+ *Last changes: 02/06/2020 13:35
+ *Task: example goroutines
  */
 
 package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"net/http"
+	"time"
 )
 
-func max(sl []string) string {
-	res := ""
-
-	for i := 0; i < len(sl); i++ {
-		if utf8.RuneCountInString(sl[i]) > utf8.RuneCountInString(res) {
-			res = sl[i]
-		}
-	}
-	return res
-}
-
-func reverse(sl []int64) []int64 {
-	rsl := []int64{}
-	for i := len(sl) - 1; i >= 0; i-- {
-		rsl = append(rsl, sl[i])
-	}
-	return rsl
-}
-
+//main func
 func main() {
-	sl := []string{"three", "one", "twelve", "stefan"}
-	sl1 := []int64{21, 41, 5, 1}
+	resources := []string{
+		"http://google.by",
+		"http://tut.by",
+		"http://vk.com",
+	}
 
-	fmt.Println(max(sl))
-	fmt.Println(reverse(sl1))
+	//create channel for communicate between goroutines
+	c := make(chan string)
+
+	//just run child goroutines
+	for _, url := range resources {
+		go checkRes(url, c)
+	}
+
+	//read
+	for l := range c {
+		go func(url string) {
+			time.Sleep(5 * time.Second)
+			checkRes(url, c)
+		}(l)
+	}
+}
+
+//checkRes func
+func checkRes(url string, c chan string) {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(url, "is down!")
+		c <- url
+		return
+	}
+
+	fmt.Println(url, "is Up!")
+	c <- url
+	return
 }
