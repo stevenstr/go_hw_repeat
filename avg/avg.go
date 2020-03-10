dd8acbb1f80c9947c1071a97253fb254022f25bd
Ste Zha <stephenzhavrid@gmail.com>
2020-03-10 23:08:38 +0300

    Add benchmark into avg pkg

--- avg/avg.go
+++ avg/avg.go
@@ -1,13 +1,13 @@
 /*
  *Author: Stefan
  *Date: 03/10/2020
- *Last changes: 03/10/2020 22:53
+ *Last changes: 03/10/2020  23:09
  *Task: Chapter 3 Lecture 3 HW 3.1 AVG
  */
 
 package avg
 
-//average func
+//Average func
 func Average(arr [6]int) (avg float64) {
 	for i := 0; i < len(arr); i++ {
 		avg += float64(arr[i])
--- avg/avg_test.go
+++ avg/avg_test.go
@@ -1,7 +1,7 @@
 /*
  *Author: Stefan
  *Date: 03/10/2020
- *Last changes: 03/10/2020 22:53
+ *Last changes: 03/10/2020 23:09
  *Task: Chapter 3 Lecture 3 HW 3.1 AVG
  */
 
@@ -24,4 +24,13 @@ func Test_AVG(t *testing.T) {
 	res1 := Average(arr1)
 
 	assert.Equal(t, 3.5, res1)
+}
+
+//Benchmark_AVG
+func Benchmark_AVG(b *testing.B) {
+	arr := [6]int{1222222, 277777, 37777, 477777, 5777777, 63333333}
+
+	for i := 0; i < b.N; i++ {
+		_ = Average(arr)
+	}
 }
